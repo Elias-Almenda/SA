b2128524c64d33b0648135a0b628601163aff747
Elias Almenda Rodrigues Padilha <eliasalmenda64@gmail.com>
2024-10-08 18:14:48 -0300

    Delete SA directory

--- SA/inicial2/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("central-button").addEventListener("click", function () {
-    var extraButtons = document.getElementById("extra-buttons");
-    if (extraButtons.style.display === "none") {
-        extraButtons.style.display = "block";
-    } else {
-        extraButtons.style.display = "none";
-    }
-});
